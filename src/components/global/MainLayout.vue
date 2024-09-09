e576ecb80656314b81a368aef3a9985b4974fe1e
naderkhaled15 <https://github.com/naderkhaled15>
2024-09-09 10:16:02 +0300

    latest

--- src/components/global/MainLayout.vue
+++ src/components/global/MainLayout.vue
@@ -2,21 +2,33 @@
   <div>
     <HeaderCom v-if="route.name !== 'signin'"></HeaderCom>
     <div class="row content">
-      <SideBar v-if="route.name !== 'signin' && !navResponse" @collapse="navResponse = true" class="col-2"
-        style="height: 100% !important"></SideBar>
+      <SideBar
+        v-if="route.name !== 'signin' && !navResponse"
+        @collapse="navResponse = true"
+        class="col-2"
+        style="height: 100% !important"
+      ></SideBar>
 
-      <SideBarCollapse @collapse="navResponse = false" :collapse="navResponse" class="col-1"
-        style="height: 100% !important; " v-if="route.name !== 'signin' && navResponse">
+      <SideBarCollapse
+        @collapse="navResponse = false"
+        :collapse="navResponse"
+        class="col-1"
+        style="height: 100% !important"
+        v-if="route.name !== 'signin' && navResponse"
+      >
       </SideBarCollapse>
 
-      <main style="overflow: hidden !important;" :class="`${route.name == 'signin' ? 'col-12' : 'col'
-        } min-vh-100 p-0 border-top `">
+      <main
+        style="overflow: hidden !important"
+        :class="`${
+          route.name == 'signin' ? 'col-12' : 'col'
+        } min-vh-100 p-0 border-top `"
+      >
         <Hint v-if="route.name !== 'signin'"></Hint>
         <div>
           <slot></slot>
         </div>
       </main>
-
     </div>
     <!-- footer -->
   </div>
@@ -31,22 +43,21 @@ import { useRoute } from "vue-router";
 import { ref, onMounted, watch } from "vue";
 
 const route = useRoute();
-const navResponse = ref(false)
-const viewWidth = ref(window.innerWidth)
+const navResponse = ref(false);
+const viewWidth = ref(window.innerWidth);
 
 const handleResize = () => {
   viewWidth.value = window.innerWidth;
 };
 
-
 onMounted(() => {
-  window.addEventListener('resize', handleResize);
-})
-
+  window.addEventListener("resize", handleResize);
+});
 
 watch(viewWidth, (newVal) => {
-  window.innerWidth <= 992 ? navResponse.value = true : navResponse.value = false
-
+  window.innerWidth <= 992
+    ? (navResponse.value = true)
+    : (navResponse.value = false);
 });
 </script>
 
--- src/components/page-component/overview/statisticals/AreaChart.vue
+++ src/components/page-component/overview/statisticals/AreaChart.vue
@@ -1,51 +1,59 @@
 <template>
-    <div class="area-chart">
-        <apexchart type="area" :options="chartOptions" :series="series"></apexchart>
-    </div>
+  <div class="area-chart">
+    <apexchart type="area" :options="chartOptions" :series="series"></apexchart>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 
-const series = ref([{
-    name: 'series1',
-    data: [31, 40, 28, 51, 42, 109, 100]
-},
-{
-    name: 'series2',
-    data: [11, 32, 45, 32, 34, 52, 41]
-},
-{
-    name: 'series3',
-    data: [15, 40, 60, 20, 30, 60, 35]
-}
-])
+const series = ref([
+  {
+    name: "series1",
+    data: [31, 40, 28, 51, 42, 109, 100],
+  },
+  {
+    name: "series2",
+    data: [11, 32, 45, 32, 34, 52, 41],
+  },
+  {
+    name: "series3",
+    data: [15, 40, 60, 20, 30, 60, 35],
+  },
+]);
 
 const chartOptions = ref({
-    chart: {
-        type: 'area'
+  chart: {
+    type: "area",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    type: "datetime",
+    categories: [
+      "2018-09-19T00:00:00.000Z",
+      "2018-09-19T01:30:00.000Z",
+      "2018-09-19T02:30:00.000Z",
+      "2018-09-19T03:30:00.000Z",
+      "2018-09-19T04:30:00.000Z",
+      "2018-09-19T05:30:00.000Z",
+      "2018-09-19T06:30:00.000Z",
+    ],
+  },
+  tooltip: {
+    x: {
+      format: "dd/MM/yy HH:mm",
     },
-    dataLabels: {
-        enabled: false
-    },
-    stroke: {
-        curve: 'smooth'
-    },
-    xaxis: {
-        type: 'datetime',
-        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-    },
-    tooltip: {
-        x: {
-            format: 'dd/MM/yy HH:mm'
-        },
-    },
-})
-
+  },
+});
 </script>
 
 <style lang="scss" scoped>
 .area-chart {
-    width: 100%;
+  width: 100%;
 }
 </style>
